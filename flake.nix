b41cd54fe93055084b95bdea21678aea0db26270
Mike O'Driscoll <mike@unusedbytes.ca>
2026-07-20 20:51:27 -0400

    flake,go.mod: update to go 1.25.6

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "prometheus exporter for plex media server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     systems.url = "github:nix-systems/default";
   };
 
